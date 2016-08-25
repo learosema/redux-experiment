84d2ace37b4c46a2c985545c4ac5b04d24056ba4
Lutz Rosema <terabaud@gmail.com>
2016-08-25 18:17:40 +0200

    simple babel/webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: "bundle.js",
+		publicPath: "/"
+	},
+	module: {
+		loaders: [
+			{ test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']},
+			{ test: /\.js$/, loader: 'babel', exclude: /(node_modules|bower_components)/, query: { cacheDirectory: true, presets: ['es2015']}}
+		]
+	},
+	postcss: function() { return [autoprefixer] }
+}

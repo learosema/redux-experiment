7d3423fcb6daf9f0aa3806c347cb3845cd210cee
Lutz Rosema <terabaud@gmail.com>
2016-10-25 13:21:06 +0200

    wip

--- src/util/random-issue.js
+++ src/util/random-issue.js
@@ -15,10 +15,13 @@ function randomThing() {
 		'Mouse',
 		'Keyboard',
 		'Data',
-		'Client'
+		'Client',
+		'File',
+		'Socket',
 	].pick() + 
 	['', 'Binding', 'Event', 'Function', 'Host', 'Window'].pick() + [
-		'EventListener',
+		'Server',
+		'Listener',
 		'Adapter',
 		'Factory',
 		'Loop',
@@ -26,11 +29,23 @@ function randomThing() {
 	].pick();
 }
 
+function deliciousThing() {
+	return ['Oreos', 'coffee', 'ice cream', 'pizza'].pick();
+}
+
+
 export default function randomIssue() {
 	return randomThing() + [
 		' throws ' + randomException(),
 		' fails to connect to ' + randomThing(),
 		' just refuses to work',
 		' causes ' + randomThing() + ' to crash'
+	].pick() + [
+		' on mondays',
+		' when you click the red button',
+		' when you try to save',
+		' because of ' + randomException(),
+		' on Windows 95',
+		' because terabaud is out of ' + deliciousThing()
 	].pick();
 }
